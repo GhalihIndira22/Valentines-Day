5c686a5d9a255a815da5a0a6b68da67bbabd7c82
galih <maulana.galih.p@gmail.com>
2024-02-13 09:23:43 +0700

    push first time
    push first time

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,21 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "Are you sure pinguin ?",
+      "Really sure pinguin boe ?",
+      "Think again pinguin boe !",
+      "Last chance pinguin botut !",
+      "Surely not bubuncitt ?",
+      "You might regret thisbubuncitt !",
+      "Give it another thought bubuncitt !",
+      "Are you absolutely certain bubuncitt?",
+      "This could be a mistake bubuncitt !",
+      "Have a heart bubuncitt !",
+      "Don't be so cold bubuncitt !",
+      "Change of heart bubuncitt botut !?",
+      "Wouldn't you reconsider pinguin botut !?",
+      "Is that your final answer pinguin botut !?",
+      "You're breaking my heart ;( pinguin botut !",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -39,16 +39,16 @@ export default function Page() {
         {yesPressed ? (
           <>
             <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <div className="text-container">Ok yay !!!</div>
           </>
         ) : (
           <>
             <img
               className="h-[200px]"
               style={{ width: "400x", height: "240px" }}
-              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+              src="https://gifdb.com/images/high/cute-sad-bubu-dudu-panda-kick-4cn5usy9xip1m59y.gif"
             />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <h1 className="text-container">Will you be my Valentine ?</h1>
             <div>
               <button
                 className={"yes-button"}
